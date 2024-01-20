5f791f9fcf248db7d80ba331a25816219f3be182
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-20 21:49:44 +0700

    02. Spring MVC Security - Custom Login Form

--- src/main/java/com/iffat/springchadmvcsecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/iffat/springchadmvcsecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.iffat.springchadmvcsecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -29,5 +31,17 @@ public class DemoSecurityConfig {
                 .roles("EMPLOYEE", "MANAGER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(john, marry, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll());
+        return http.build();
     }
 }
